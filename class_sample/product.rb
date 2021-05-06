e38973d77446fd2d2ca1da6a266b227e17fe725a
kurokawakazuhiro <k.kurokawa11@gmail.com>
2021-05-06 09:30:44 +0900

    クラスインスタンス変数とインスタンス変数の違いを学習

---
+++ class_sample/product.rb
@@ -0,0 +1,19 @@
+class Product
+  # クラスインスタンス変数
+  @name = 'Product'
+
+  def self.name
+    # クラスインスタンス変数
+    @name
+  end
+
+  def initialize(name)
+    # インスタンス変数
+    @name = name
+  end
+
+  def name
+    # インスタンス変数
+    @name
+  end
+end
